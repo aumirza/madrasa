db61b06f323ef76ed299f94d3ce245e29e863280
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-26 15:46:03 +0530

    refactor: improve clarity of DebugConsole component by adding development mode check comment

--- src/components/DebugConsole.tsx
+++ src/components/DebugConsole.tsx
@@ -4,7 +4,11 @@ import { useEffect } from 'react';
 
 export default function DebugConsole() {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    // Only show debug console in development mode
+    if (
+      process.env.NODE_ENV === 'development' &&
+      typeof window !== 'undefined'
+    ) {
       const VConsole = require('vconsole');
       new VConsole();
     }

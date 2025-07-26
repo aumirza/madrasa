4a1a267a3d563ab7496fda88cd30d43c7d8da6b8
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-26 13:31:35 +0530

    refactor: comment out error handling in getLocationFromIp function

--- src/lib/IPLocation.ts
+++ src/lib/IPLocation.ts
@@ -2,9 +2,9 @@ export const getLocationFromIp = async (
   ip: string
 ): Promise<{ longitude: number; latitude: number }> => {
   const response = await fetch(`https://ipapi.co/${ip}/json/`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch location data');
-  }
+  // if (!response.ok) {
+  //   throw new Error('Failed to fetch location data');
+  // }
   const data = await response.json();
   return {
     // country: data.country_name || 'Unknown',
